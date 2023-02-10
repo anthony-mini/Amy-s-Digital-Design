ddc225461ce0bf1ed59e9e8de97f79793ea209a5
anthony-mini <anthony-mini@outlook.fr>
2023-02-10 22:11:41 +0100

    Final responsive work

--- src/components/BookMe/index.jsx
+++ src/components/BookMe/index.jsx
@@ -4,12 +4,15 @@ import Button from '../../components/Button/Button_2';
 export default function BookMe() {
   return (
     <section className="section-5">
+      <div className='BookMe-txt'>
+
       <h2>Book a call with me</h2>
       <p>
         I’d love to have a chat to see how I can help you. The best first step
         is for us to discuss your project during a free consultation. Then we
         can move forward from there.
       </p>
+      </div>
       <Button />
     </section>
   );
