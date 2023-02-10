7fcaafbf4e007330a573bd701c31a46f0fad19d6
anthony-mini <anthony-mini@outlook.fr>
2023-02-10 19:21:43 +0100

    Responsive Work

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,13 +6,15 @@ export default function About() {
   return (
     <section className="section-3">
       <img src={ImgProfil} alt="pictu amy" />
-      <h2> I’m Amy, and I’d love to work on your next project </h2>
-      <p className="paragraph">
-        I love working with others to create beautiful design solutions. I’ve
-        designed everything from brand illustrations to complete mobile apps.
-        I’m also handy with a camera!
-      </p>
-      <Button />
+      <div className="section-3__bloc">
+        <h2> I’m Amy, and I’d love to work on your next project </h2>
+        <p className="paragraph">
+          I love working with others to create beautiful design solutions. I’ve
+          designed everything from brand illustrations to complete mobile apps.
+          I’m also handy with a camera!
+        </p>
+        <Button />
+      </div>
     </section>
   );
 }
