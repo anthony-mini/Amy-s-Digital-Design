e2ebb38cd884c4181285b45ac8798c1bc13a65b0
anthony-mini <anthony-mini@outlook.fr>
2023-02-10 17:13:53 +0100

    Add component Slider to Component project

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Gallery from '../Gallery';
+
+export default function Projects() {
+  return (
+    <section className="section-4">
+      <h2>My Work</h2>
+      <Gallery />
+    </section>
+  );
+}
