fbb3f248f6a002a2fe0a1e7111a6507fd044ff17
anthony-mini <anthony-mini@outlook.fr>
2023-02-10 15:08:34 +0100

    Add new components : `About`

---
+++ src/components/About/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ImgProfil from '../../assets/image-amy.webp';
+import Button from '../../components/Button/Button_2';
+
+export default function About() {
+  return (
+    <section className="section-3">
+      <img src={ImgProfil} alt="pictu amy" />
+      <h2> I’m Amy, and I’d love to work on your next project </h2>
+      <p className="paragraph">
+        I love working with others to create beautiful design solutions. I’ve
+        designed everything from brand illustrations to complete mobile apps.
+        I’m also handy with a camera!
+      </p>
+      <Button />
+    </section>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 // import Button1 from '../../components/Button/Button_1';
 import Header from '../../components/Header';
 import Disciplines from '../../components/Disciplines';
+import About from '../../components/About';
 import Footer from '../../components/Footer';
-
 
 export default function Home() {
   useEffect(() => {
@@ -14,14 +14,17 @@ export default function Home() {
     <React.Fragment>
       <Header />
       <main>
-        <section className='section-1'>
-          <h1 className=" section-1--headling headling">Design solutions made easy</h1>
+        <section className="section-1">
+          <h1 className=" section-1--headling headling">
+            Design solutions made easy
+          </h1>
           <p className="section-1--paragraph paragraph ">
             With over ten years of experience in various design disciplines, I’m
             your one-stop shop for your design needs.
           </p>
         </section>
         <Disciplines />
+        <About />
       </main>
       <Footer />
     </React.Fragment>
