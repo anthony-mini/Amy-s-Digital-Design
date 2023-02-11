c672269942540afde644554dcac825d48ba8cbd5
anthony-mini <anthony-mini@outlook.fr>
2023-02-11 16:14:05 +0100

    resolve HTML error

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,7 +4,7 @@ import Button from '../../components/Button/Button_2';
 
 export default function About() {
   return (
-    <section className="section-3">
+    <div className="section-3">
       <img src={ImgProfil} alt="pictu amy" />
       <div className="section-3__bloc">
         <h2> I’m Amy, and I’d love to work on your next project </h2>
@@ -15,6 +15,6 @@ export default function About() {
         </p>
         <Button />
       </div>
-    </section>
+    </div>
   );
 }
