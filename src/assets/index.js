1d0c27cd411e50ad040145ef6e520d21b61f0239
anthony-mini <anthony-mini@outlook.fr>
2023-02-21 22:45:47 +0100

    Clean Code : Regroupe les assets dans un fichier index"

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+export { default as ArrowLeft } from './icon-arrow-left.svg';
+export { default as ArrowRight } from './icon-arrow-right.svg';
+
+export { default as Apps } from './pattern-apps.svg';
+export { default as Graphic } from './pattern-graphic-design.svg';
+export { default as Illustration } from './pattern-illustrations.svg';
+export { default as MotionGraph } from './pattern-motion-graphics.svg';
+export { default as Photography } from './pattern-photography.svg';
+export { default as UxUi } from './pattern-ui-ux.svg';
