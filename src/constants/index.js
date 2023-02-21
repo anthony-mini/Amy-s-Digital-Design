9a6c4be827e6a33a234a1e61c0e891f123a198e3
anthony-mini <anthony-mini@outlook.fr>
2023-02-21 22:46:34 +0100

    Clean Code : Regroupe toutes les constantes dans un fichier index

---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+export const ArrayImg = [
+  {
+    id: 1,
+    src: require('../assets/image-slide-1.jpg'),
+    alt: 'Image 1',
+  },
+  {
+    id: 2,
+    src: require('../assets/image-slide-2.jpg'),
+    alt: 'Image 2',
+  },
+  {
+    id: 3,
+    src: require('../assets/image-slide-3.jpg'),
+    alt: 'Image 3',
+  },
+  {
+    id: 4,
+    src: require('../assets/image-slide-4.jpg'),
+    alt: 'Image 4',
+  },
+  {
+    id: 5,
+    src: require('../assets/image-slide-5.jpg'),
+    alt: 'Image 5',
+  },
+];
